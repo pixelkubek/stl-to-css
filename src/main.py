9e9cc59e9e9ffc7c966e378fa91682b1050d5943
pixelkubek <150136485+pixelkubek@users.noreply.github.com>
2025-04-08 20:41:35 +0200

    Switch to numpy and add rotation matrices

--- src/main.py
+++ src/main.py
@@ -1,34 +1,42 @@
 from dataclasses import dataclass
+from math import sin, cos
 
-@dataclass
-class vector_3d:
-    x: float
-    y: float
-    z: float
+import numpy as np
 
-    def __add__(self, other):
-        return vector_3d(self.x + other.x, self.y + other.y, self.z + other.z)
-    
-    def __iadd__(self, other):
-        self.x += other.x
-        self.y += other.y
-        self.z += other.z
+def create_x_rotation_matrix(angle_radians: float) -> np.ndarray:
+    return np.array(
+        [1, 0, 0],
+        [0, cos(angle_radians), -sin(angle_radians)],
+        [0, sin(angle_radians), cos(angle_radians)]
+    )
 
-    def __sub__(self, other):
-        return vector_3d(self.x - other.x, self.y - other.y, self.z - other.z)
-    
-    def __isub__(self, other):
-        self.x -= other.x
-        self.y -= other.y
-        self.z -= other.z
+def create_y_rotation_matrix(angle_radians: float) -> np.ndarray:
+    return np.array(
+        [cos(angle_radians), 0, sin(angle_radians)],
+        [0, 1, 0],
+        [-sin(angle_radians), 0, cos(angle_radians)]
+    )
+
+def create_z_rotation_matrix(angle_radians: float) -> np.ndarray:
+    return np.array(
+        [cos(angle_radians), -sin(angle_radians), 0],
+        [sin(angle_radians), cos(angle_radians), 0],
+        [0, 0, 1]
+    )
 
 @dataclass
 class triangle_3d:
-    vertices: tuple[vector_3d, vector_3d, vector_3d]
+    vertices: tuple[np.ndarray, np.ndarray, np.ndarray]
 
     def origin_representation(self):
         p1, p2, p3 = self.vertices
         return triangle_3d(vertices=(p1 - p1, p2 - p1, p3 - p1))
 
 
+my_trig = triangle_3d((
+    np.array([0, 0, 0]),
+    np.array([-100, 0, 0]),
+    np.array([-50, -100, 0])
+))
 
+print(my_trig)
