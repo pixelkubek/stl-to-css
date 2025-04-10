fffec7d0d2c38c61f53cfc8adb44687fcc3dad69
pixelkubek <150136485+pixelkubek@users.noreply.github.com>
2025-04-10 19:53:24 +0200

    update wrap html

--- src/html.py
+++ src/html.py
@@ -1,5 +1,5 @@
-def wrap_http(body: str = "", type: str = "div", **kwargs) -> str:
-    attributes = []
-    for key, val in kwargs:
-        attributes.append(f"{key}=\"{val}\"")
-    return f"<div {" ".join(attributes)}>{body}</div>"
+def wrap_html(type: str, body: str = "", attributes: dict = dict()) -> str:
+    attributes_list = []
+    for key, val in attributes.items():
+        attributes_list.append(f"{key}=\"{val}\"")
+    return f"<{type} {" ".join(attributes_list)}>{body}</{type}>"
