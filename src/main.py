57a750f0517b7bf8f0eece8e0520bec6fa0b130e
pixelkubek <150136485+pixelkubek@users.noreply.github.com>
2025-04-25 15:44:17 +0200

    Added if name == main

--- src/main.py
+++ src/main.py
@@ -4,22 +4,23 @@ from object3d import object_3d, read_stl_file
 
 import sys
 
-obj = read_stl_file('pikachu.stl', 'obj')
-obj.fit_to_size(50)
+if __name__ == '__main__':
+    obj = read_stl_file('pikachu.stl', 'obj')
+    obj.fit_to_size(50)
 
-html, css = html_css_of_object3d(obj, 50, svg_pixel_scale = 10, color='yellow', unit='vh')
-# print(obj, file=sys.stderr)
+    html, css = html_css_of_object3d(obj, 50, svg_pixel_scale = 10, color='yellow', unit='vh')
+    # print(obj, file=sys.stderr)
 
-print(f'''<!DOCTYPE html>
-<html>
-    <head>
-        <link rel="stylesheet" href="test.css">
-        <style>
-            {css}
-        </style>
-    </head>
-    <body>
-        {html}
-    </body>
-</html>
-''')
+    print(f'''<!DOCTYPE html>
+    <html>
+        <head>
+            <link rel="stylesheet" href="test.css">
+            <style>
+                {css}
+            </style>
+        </head>
+        <body>
+            {html}
+        </body>
+    </html>
+    ''')
